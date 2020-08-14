89acb54d688f3254acb87db900f183e39d14245c
Myloveiszero <2446730006@qq.com>
2020-08-14 17:12:28 +0800

    webpack

---
+++ babel/app.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+const sum = 2**2
+function observre() {
+
+}
+@observre
+class App extends Component {
+
+    render() {
+        return(
+            <div>
+            <img src="baidu.com" alt="baidu" className="box"/>
+             <li></li>
+           </div>
+        )
+    }
+    
+}
+export default App;
---
+++ babel/src/app.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+const sum = 2**2
+function observre() {
+
+}
+@observre
+class App extends Component {
+
+    render() {
+        return(
+            <div>
+            <h1>This is a page</h1>
+           </div>
+        )
+    }
+    
+}
+export default App;
